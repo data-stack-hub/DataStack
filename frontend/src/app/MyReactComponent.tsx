bf071b52063d219fe61bf607a4d913e13f9799bc
Vishal Vora <vvora@enovix.com>
2023-02-18 11:05:03 +0530

    -

--- frontend/src/app/MyReactComponent.tsx
+++ frontend/src/app/MyReactComponent.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import { FunctionComponent, useEffect, useRef, useState } from 'react';
+import {Client as Styletron} from 'styletron-engine-monolithic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider, styled } from 'baseui';
+import { StatefulInput } from 'baseui/input';
+// import Button from '@material-ui/core/Button'; 
+import { Button } from "baseui/button";
 
-import Button from '@material-ui/core/Button'; 
 import ReactJson from 'react-json-view'
 // import ReactMarkdown from 'react-markdown'
 export interface IMyComponentProps {
@@ -28,6 +33,7 @@ export const MyReactComponent: FunctionComponent<IMyComponentProps> = (props: IM
   });
 
   const {counter: propsCounter, onClick} = props;
+  const engine = new Styletron();
   const my_json_object = {
     "a":"a",
     b:{c:"d"}
@@ -46,7 +52,14 @@ export const MyReactComponent: FunctionComponent<IMyComponentProps> = (props: IM
       <span onClick={handleClick}
             className={'increase-button'}>click to increase</span>
     </div>
-    <Button variant="contained" onClick={handleClick}>Click</Button>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        {/* <Centered> */}
+          <StatefulInput />
+        {/* </Centered> */}
+      </BaseProvider>
+    </StyletronProvider>
+    <Button  onClick={handleClick}>Click</Button>
     <div className={'comp-state'}>State counter: {stateCounter}</div>
     <ReactJson 
     src={my_json_object} 
