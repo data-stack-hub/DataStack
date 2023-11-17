3c57946ebbb047d311ef92ecb3b46e5bc4df9ebb
Vishal Vora <vvora@enovix.com>
2023-11-17 11:13:43 +0530

    docs

--- lib/setup.py
+++ lib/setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="Tool to build data apps on the fly",
     entry_points={"console_scripts":["datastack = datastack.cli:main"]},
     install_requires=['click==8.1.3','Flask==2.2.2','Flask_Cors==3.0.10','GitPython==3.1.31','numpy','pandas',
-                      'plotly==5.14.0','requests==2.28.1'],
+                      'plotly==5.14.0','requests==2.28.1', 'sphinx_rtd_theme'],
     data_files=[('static', findall('datastack/static/'))],                      
     include_package_data=True,
    
