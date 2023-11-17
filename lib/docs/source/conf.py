cd09b79b7fff634d4f9c1a388380cd85a9485c14
Vishal Vora <vvora@enovix.com>
2023-11-17 12:35:10 +0530

    docs

--- lib/docs/source/conf.py
+++ lib/docs/source/conf.py
@@ -7,7 +7,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.abspath("../.."), 'lib'))
+sys.path.insert(0, os.path.abspath("../.."), 'lib')
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
