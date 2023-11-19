c08e56990f838d0bae810a257620061d0533f1b4
Vishal Vora <vvora@enovix.com>
2023-11-19 11:01:48 +0530

    dependency update

--- lib/setup.py
+++ lib/setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="The Fastes way to build apps in python",
     entry_points={"console_scripts":["datastack = datastack.cli:main"]},
     install_requires=['click==8.1.3','Flask==2.2.2','Flask_Cors==3.0.10','GitPython==3.1.31','numpy','pandas',
-                      'plotly==5.14.0','requests==2.28.1', 'sphinx_rtd_theme'],
+                      'plotly==5.14.0','requests==2.28.1', 'cachetools', 'varname', 'Werkzeug==2.2.2', 'waitress'],
     data_files=[('static', findall('datastack/static/'))],                      
     include_package_data=True,
     setup_requires=['wheel']
