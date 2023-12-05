3d50efb68f0b6ae752967cf38e64747ba2bfc2f3
Vishal Vora <vvora@enovix.com>
2023-12-05 11:08:55 +0530

    gh actions

--- lib/setup.py
+++ lib/setup.py
@@ -3,15 +3,28 @@ from setuptools import findall
 from distutils.core import setup
 
 setuptools.setup(
-    name='pydatastack',
-    version='0.0.2',
-    author="Vishal Vora, Mayur Pokiya, Karan Doshi", 
+    name="pydatastack",
+    version="0.0.4",
+    author="Vishal Vora, Mayur Pokiya, Karan Doshi",
     description="The Fastes way to build apps in python",
-    entry_points={"console_scripts":["datastack = datastack.cli:main"]},
-    install_requires=['click==8.1.3','Flask==2.2.2','Flask_Cors==3.0.10','GitPython==3.1.31','numpy','pandas',
-                      'plotly==5.14.0','requests==2.28.1', 'cachetools', 'varname','asttokens','pure_eval', 'Werkzeug==2.2.2', 'waitress'],
-    data_files=[('static', findall('datastack/static/'))],                      
+    entry_points={"console_scripts": ["datastack = datastack.cli:main"]},
+    install_requires=[
+        "click==8.1.3",
+        "Flask==2.2.2",
+        "Flask_Cors==3.0.10",
+        "GitPython==3.1.31",
+        "numpy",
+        "pandas",
+        "plotly==5.14.0",
+        "requests==2.28.1",
+        "cachetools",
+        "varname",
+        "asttokens",
+        "pure_eval",
+        "Werkzeug==2.2.2",
+        "waitress",
+    ],
+    data_files=[("static", findall("datastack/static/"))],
     include_package_data=True,
-    setup_requires=['wheel']
-   
+    setup_requires=["wheel"],
 )
